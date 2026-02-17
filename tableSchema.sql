7392c5accc27b293c63d57113c3b8c42a8849cb1
quantse7en <manojnquant@gmail.com>
2026-02-17 23:10:43 +0530

    Add schema for creating a centralized database exchange

---
+++ tableSchema.sql
@@ -0,0 +1,59 @@
+--clickHouse files
+
+--Save Equity data from df from KITE API python functions
+
+CREATE TABLE mkt.equity_1min
+(
+    symbol String,
+    instrument_token UInt32,
+    exchange LowCardinality(String),
+    timestamp DateTime,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    volume UInt64
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMM(timestamp)
+ORDER BY (symbol, timestamp);
+
+
+--Save Futures data from df from KITE API python functions
+CREATE TABLE mkt.futures_1min
+(
+    symbol String,
+    instrument_token UInt32,
+    expiry Date,
+    timestamp DateTime,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    volume UInt64,
+    oi UInt64
+)
+ENGINE = MergeTree()
+PARTITION BY (toYYYYMM(timestamp), expiry)
+ORDER BY (symbol, expiry, timestamp);
+
+
+--Save Options data from df from KITE API python functions
+CREATE TABLE mkt.options_1min
+(
+    symbol String,
+    instrument_token UInt32,
+    expiry Date,
+    strike Float64,
+    optType LowCardinality(String), -- CE / PE
+    timestamp DateTime,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    volume UInt64,
+    oi UInt64
+)
+ENGINE = MergeTree()
+PARTITION BY (toYYYYMM(timestamp), expiry)
+ORDER BY (symbol, expiry, strike, optType, timestamp);
